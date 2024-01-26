3857b03d599ad4794ccd12b9399a6f405ee80e8c
Nicolas Pawelka <pawelka12@gmail.com>
2024-01-26 10:21:30 +0100

    Update

--- Polarion/API.py
+++ Polarion/API.py
@@ -1,6 +1,8 @@
 from polarion import polarion
 client = polarion.Polarion('https://polarion-dev2.newtec.zz/polarion','PawelkaN','KORRNiggelus3713.,.,',verify_certificate=False)
-project = client.getProject('Python')
-#workitem = project.getWorkitem('PYTH-510')
+project = client.getProject('Spielprojekt_IEC62443')
+workitem = project.getWorkitem('SIEC-1899')
+print("###############################")
+print(workitem)
 #run = project.getTestRun('SWQ-0001')
 #plan = project.getPlan('00002')
