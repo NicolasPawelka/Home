f4704f5bd3abaffcbf60c57c28b9f340cc28cdd4
Pawelka Nicolas <Nicolas.Pawelka@newtec.de>
2024-03-15 12:24:00 +0100

    Addeed Zeiterfassung

---
+++ rust/hello_world/target/debug/deps/hello_world-65c478c245451323.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/npawelka/Desktop/repo/Home/rust/hello_world/target/debug/deps/hello_world-65c478c245451323: src/main.rs
+
+/mnt/c/Users/npawelka/Desktop/repo/Home/rust/hello_world/target/debug/deps/hello_world-65c478c245451323.d: src/main.rs
+
+src/main.rs:
