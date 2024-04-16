7e945f799c3850d374008c82adefd88f233f33ef
Nicolas <2120666@hs-mannheim.de>
2024-04-16 12:17:59 +0200

    Rotation über User Matrix realisiert. Praktikum_3 fertig

--- Sem6/BM/Praktikum_3.py
+++ Sem6/BM/Praktikum_3.py
@@ -6,8 +6,27 @@ Created on Tue Apr 16 10:58:00 2024
 """
 
 from vtk import vtkCylinderSource, vtkPolyDataMapper, vtkActor, vtkRenderer
-from vtk import vtkRenderWindow, vtkRenderWindowInteractor
+from vtk import vtkRenderWindow, vtkRenderWindowInteractor, vtkMatrix4x4
 from vtkmodules.vtkCommonColor import vtkNamedColors
+import numpy as np
+
+def getRotateBitch(winkel,axis):
+    
+    matrix = np.eye(4)
+    vtk_matrix = vtkMatrix4x4()
+    
+
+    
+    if(axis == "x"):
+        matrix [1,1:3] = [np.cos(winkel), -1*np.sin(winkel)]
+        matrix[2,1:3] = [np.sin(winkel), np.cos(winkel)]
+    
+    for i in range(4):
+        for j in range (4):
+            vtk_matrix.SetElement(i,j,matrix[i][j])
+    
+    
+    return vtk_matrix
 
 
 farbe = input("Bitte gewünschte Farbe des Zylinders eingeben: ")
@@ -32,7 +51,7 @@ mapper = vtkPolyDataMapper()
 mapper.SetInputConnection(cylinderSource.GetOutputPort())
 actor = vtkActor()
 actor.GetProperty().SetColor(colors.GetColor3d(farbe))
-actor.RotateX(45)
+actor.SetUserMatrix(getRotateBitch(45, "x"))
 actor.SetMapper(mapper)
 
 mapper_new = vtkPolyDataMapper()
@@ -41,6 +60,7 @@ actor_new = vtkActor()
 actor_new.GetProperty().SetColor(colors.GetColor3d(farbe))
 actor_new.RotateY(45)
 actor_new.SetMapper(mapper_new)
+
 
 
 renderer = vtkRenderer()
@@ -59,7 +79,7 @@ renderer.SetBackground(colors.GetColor3d('DarkGreen'))
 renderWindow.Render()
 renderWindowInteractor.Start()
 
-
+    
 
 
 
