7bd8b94b916e9ed84e79e8d2b61da322fef49b9d
Nicolas <2120666@hs-mannheim.de>
2024-04-16 11:50:54 +0200

    Zweiter Zylinder hinzugefügt und beide um 45 Grad rotiert

--- Sem6/BM/Praktikum_3.py
+++ Sem6/BM/Praktikum_3.py
@@ -19,13 +19,28 @@ cylinderSource.SetRadius(0.5)
 cylinderSource.SetHeight(5.0)
 cylinderSource.SetResolution(100)
 
+cylinder_new = vtkCylinderSource()
+cylinder_new.SetCenter(5.0,0.0,0.0)
+cylinder_new.SetRadius(0.5)
+cylinder_new.SetHeight(5.0)
+cylinder_new.SetResolution(100)
+
+
 colors = vtkNamedColors()
 
 mapper = vtkPolyDataMapper()
 mapper.SetInputConnection(cylinderSource.GetOutputPort())
 actor = vtkActor()
 actor.GetProperty().SetColor(colors.GetColor3d(farbe))
+actor.RotateX(45)
 actor.SetMapper(mapper)
+
+mapper_new = vtkPolyDataMapper()
+mapper_new.SetInputConnection(cylinder_new.GetOutputPort())
+actor_new = vtkActor()
+actor_new.GetProperty().SetColor(colors.GetColor3d(farbe))
+actor_new.RotateY(45)
+actor_new.SetMapper(mapper_new)
 
 
 renderer = vtkRenderer()
@@ -36,7 +51,9 @@ renderWindowInteractor = vtkRenderWindowInteractor()
 renderWindowInteractor.SetRenderWindow(renderWindow)
 
 renderer.AddActor(actor)
+renderer.AddActor(actor_new)
 #renderer.SetBackground(colors.GetColor3d('DarkGreen'))
+
 
 
 renderWindow.Render()
