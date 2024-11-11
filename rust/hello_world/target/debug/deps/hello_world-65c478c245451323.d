e9ecc3beefa25dd6960dff732388d3a368b8562b
Nicolas <2120666@hs-mannheim.de>
2024-11-11 17:36:35 +0100

    Deleted Rust and updated Drawio

--- rust/hello_world/target/debug/deps/hello_world-65c478c245451323.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/npawelka/Desktop/repo/Home/rust/hello_world/target/debug/deps/hello_world-65c478c245451323: src/main.rs
-
-/mnt/c/Users/npawelka/Desktop/repo/Home/rust/hello_world/target/debug/deps/hello_world-65c478c245451323.d: src/main.rs
-
-src/main.rs:
