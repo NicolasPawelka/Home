84266c548451257249d5bc0bda7e712deadb848c
Nicolas <2120666@hs-mannheim.de>
2024-11-12 07:11:22 +0100

    Deleetd rust dir

--- rust/hello_world/target/debug/deps/hello_world-df853cdafd9ed411.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/npawelka/Desktop/repo/Home/rust/hello_world/target/debug/deps/hello_world-df853cdafd9ed411: src/main.rs
-
-/mnt/c/Users/npawelka/Desktop/repo/Home/rust/hello_world/target/debug/deps/hello_world-df853cdafd9ed411.d: src/main.rs
-
-src/main.rs:
